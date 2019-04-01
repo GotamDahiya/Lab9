bd0f213d84ec39ca2793635aeba21e9232036fab
Gotam Dahiya <44541256+GotamDahiya@users.noreply.github.com>
2019-04-02 00:01:33 +0530

    Q5

---
+++ 5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main(void)
+{
+	int l;
+	scanf("%d",&l);
+	char str[l];
+	scanf("%s",str);
+	for(int i=1;i<(l-1);i++)
+	{
+		for(int j=1;j<(l-1);j++)
+		{
+			if(str[j]!=str[j+1] && str[j]==str[j-1])
+			{
+				char ch1=str[j];
+				str[j]=str[j+1];
+				str[j+1]=ch1;
+			}
+		}
+	}
+	for(int i=l-1;i>0;i--)
+	{
+		for(int j=l-1;j>0;j++)
+		{
+			if(str[j]!=str[j-1] && str[j]==str[j+1])
+			{
+				char ch2=
+			}
+		}
+	}
+	for (int i = 0; i < l; ++i)
+	{
+		printf("%c",str[i]);
+	}
+	printf("\n");
+	return 0;
+}
