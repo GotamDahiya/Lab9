675a25a48c97a3022e404afc009bb9cfda12a080
GotamDahiya <apollo2562000@gmail.com>
2019-04-03 02:40:18 +0530

    5.c

--- 5.c
+++ 5.c
@@ -7,8 +7,8 @@ int main(void)
 	scanf("%d",&l);
 	char str[l];
 	scanf("%s",str);
-	for(int i=1;i<(l-1);i++)
-	{
+	//for(int i=1;i<(l-1);i++)
+	//{
 		for(int j=1;j<(l-1);j++)
 		{
 			if(str[j]!=str[j+1] && str[j]==str[j-1])
@@ -18,17 +18,19 @@ int main(void)
 				str[j+1]=ch1;
 			}
 		}
-	}
-	for(int i=l-1;i>0;i--)
-	{
-		for(int j=l-1;j>0;j++)
+	//}
+	// for(int i=l-2;i>0;i--)
+	// {
+		for(int j=l-2;j>0;j--)
 		{
 			if(str[j]!=str[j-1] && str[j]==str[j+1])
 			{
-				char ch2=
+				char ch2=str[j];
+				str[j]=str[j-1];
+				str[j-1]=ch2;
 			}
 		}
-	}
+	//}
 	for (int i = 0; i < l; ++i)
 	{
 		printf("%c",str[i]);
