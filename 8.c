df9bccf07009c73df53e928c72c109563d61ea39
GotamDahiya <apollo2562000@gmail.com>
2019-04-03 15:28:00 +0530

    Q8

--- 8.c
+++ 8.c
@@ -1,38 +1,15 @@
+//use greedy alogorithm
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
-struct project
-{
-	int p;
-	int c;
-};
-void push(struct project arr[],int a,int b,int i)
-{
-	arr[i].p=a;
-	arr[i].c=b;
-}
-int capital(struct project arr[],int w,int n,int k)
-{
-	int start=0;
-	int end=1;
-	w=arr[start].c-w;
-	while(w<0 || end!=start && i<k)
-	{
-		int i=0;
-		
-		i++;
-	}
-}
 int main(void)
 {
 	int k,w,n;
-	scanf("%d %d %d",&k,&w,&n);
-	struct project arr[n];
+	scanf("%d %d %d",&k,&n,&w);
+	int p[n];
+	int c[n];
 	for(int i=0;i<n;i++)
 	{
-		int a,b;
-		scanf("%d %d",&a,&b);
-		push(arr,a,b,i);
+
 	}
-	int v=capital(arr,w,n,k);
 }
